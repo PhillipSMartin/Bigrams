9d8e9417fac347ca3159d72dd8e842fdde9934b6
Phillip Martin <sarabandoid@gmail.com>
2025-01-02 21:56:24 -0500

    Created tensor with counts of each bigram

--- main.py
+++ main.py
@@ -1,39 +1,52 @@
-import torch, sys
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.colors
+import matplotlib.pyplot as plt
+import torch
 
 # create a list of all chorales
 with open('all-chorales-clean.txt', 'r') as file:
-    # chorale will hold an item for each chorale
-    chorales = file.read().splitlines()
+    # [SOC] and [EOC] will both be changed to "."; [EOM] will be changed to "/"
+    chorales = (file.read()
+        .replace("[SOC]",".")
+        .replace("[EOC]",".")
+        .replace("[EOM]","/")
+        .splitlines())
 
     # tokens will hold an item for each unique token
-    # [SOC] and [EOC] will both be changed to "."
+    # [SOC] and [EOC] will both be changed to "."; [EOM] will be changed to "/"
     file.seek(0)
-    tokens = set(file.read()
+    vocabulary = set(file.read()
                 .replace('\n', ' ')
                 .replace("[SOC]",".")
                 .replace("[EOC]",".")
+                .replace("[EOM]","/")
                 .split())
-
-print(f"We have {len(tokens)} tokens in our vocabulary.")
 
 # change tokens from a set to a list and sort it alphabetically
 # the index of a token into this list will be the token identifier
-tokens = sorted(list(tokens))
-print(tokens[:10])
+vocabulary = sorted(list(vocabulary))
+stoi = {s:i for i, s in enumerate(vocabulary)}
 
-# create a tensor to hold the number of times each token is followed by a particular token
-torch.zeros((len(tokens), len(tokens)), dtype=torch.int32)
+# create a tensor to hold the number of times each token in the row is followed by the token in the column
+N = torch.zeros((len(vocabulary), len(vocabulary)), dtype=torch.int32)
 
-# create a t with count for each bigram
-b = {}
 for c in chorales:
     # split chorale into tokens
     tokens = c.split()
     # zip tokens and tokens[1:] to create tuples of each token and its successor
     for t1, t2 in zip(tokens, tokens[1:]):
-        # use the tuple as a key in the dictionary and count occurences
-        bigram = (t1, t2)
-        b[bigram] = b.get(bigram, 0) + 1
 
-sb = sorted(b.items(), key = lambda kv: -kv[1])
-print ( sb[:10] )
+        # increment the associated entry in N
+        ix1 = stoi[t1]
+        ix2 = stoi[t2]
+        N[ix1, ix2] += 1
+
+# show N via matplotlib
+# must run sudo apt-get install python3-tk to run outside a Jupyter notebook
+# need logarithmic scale to see anything
+plt.imshow(N, norm=matplotlib.colors.LogNorm())
+plt.show()
+
+
+
