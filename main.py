0d713c6a3c9308c67ac00d9df89c388e65f7fc4a
Phillip Martin <sarabandoid@gmail.com>
2025-01-20 22:59:22 -0500

    Print some statistics on tokens

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.colors
-import matplotlib.pyplot as plt
 import torch
 
 # create a list of all chorales
-with open('all-chorales-clean.txt', 'r') as file:
-    # [SOC] and [EOC] will both be changed to "."; [EOM] will be changed to "/"
+with open('all-chorales-chords-noeom.txt', 'r') as file:
     chorales = (file.read()
-        .replace("[SOC]",".")
-        .replace("[EOC]",".")
-        .replace("[EOM]","/")
         .splitlines())
 
     # tokens will hold an item for each unique token
-    # [SOC] and [EOC] will both be changed to "."; [EOM] will be changed to "/"
     file.seek(0)
-    vocabulary = set(file.read()
-                .replace('\n', ' ')
-                .replace("[SOC]",".")
-                .replace("[EOC]",".")
-                .replace("[EOM]","/")
-                .split())
+    all_tokens = file.read().split()
+    vocabulary = set(all_tokens)
+    print(f"All tokens: {len(all_tokens)}")
+    print(f"Unique tokens: {len(vocabulary)}")
 
 # change tokens from a set to a list and sort it alphabetically
 # the index of a token into this list will be the token identifier
 vocabulary = sorted(list(vocabulary))
-stoi = {s:i for i, s in enumerate(vocabulary)}
+vocabulary.remove('.') # we want to ensure the identifer for '.' is 0
+stoi = {s:i+1 for i, s in enumerate(vocabulary)}
+stoi['.'] = 0
+# create a reverse lookup table
+itos = {i:s for s, i in stoi.items()}
 
 # create a tensor to hold the number of times each token in the row is followed by the token in the column
-N = torch.zeros((len(vocabulary), len(vocabulary)), dtype=torch.int32)
+# we removed '.' from the vocabulary so we must add 1 to the count
+N = torch.zeros((len(vocabulary) + 1, len(vocabulary) + 1), dtype=torch.int32)
 
 for c in chorales:
     # split chorale into tokens
@@ -42,11 +36,10 @@ for c in chorales:
         ix2 = stoi[t2]
         N[ix1, ix2] += 1
 
-# show N via matplotlib
-# must run sudo apt-get install python3-tk to run outside a Jupyter notebook
-# need logarithmic scale to see anything
-plt.imshow(N, norm=matplotlib.colors.LogNorm())
-plt.show()
+sum_N = N.sum(dim=1)
+sorted_values = sum_N.sort()[0]  # sort the values
 
+print(f'Number of singular tokens: {(sum_N == 1).sum().item()}')
+print(f'Counf of twenty most common tokens: {sorted_values[-20:].tolist()}')
 
 
